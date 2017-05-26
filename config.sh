fac35e3549d97f45ce5d276a5664d4e83547db83
Matthew Brett <matthew.brett@gmail.com>
2017-05-26 21:07:12 +0100

    Make py.test test installed package.

--- config.sh
+++ config.sh
@@ -19,5 +19,5 @@ function run_tests {
     # Runs tests on installed distribution from an empty directory
     python --version
     echo "backend : agg" > matplotlibrc
-    py.test -v --doctest-modules skimage
+    py.test -v --doctest-modules --pyargs skimage
 }
