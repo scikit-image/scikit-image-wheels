e66b25f4b5e2149e7df494dea36acb9849108291
Gregory Lee <grlee77@gmail.com>
2020-10-05 16:15:12 -0400

    provide concrete example in rename_wheel comments

--- extra_functions.sh
+++ extra_functions.sh
@@ -1,10 +1,13 @@
 # wheel renaming utility copied from github.com/MacPython/scipy-wheels
 
 function rename_wheel {
-    # Call with a name like scipy-1.5.0.dev0+58dbafa-cp37-cp37m-linux_x86_64.whl
+    # Call with a name like
+    #      scikit_image-0.18.0.dev0+58dbafa-cp37-cp37m-linux_x86_64.whl
+    # And a new name with an inserted date will be generated. For example:
+    #      scikit_image-0.18.0.dev0+20201005161318_58dbafa-cp37-cp37m-linux_x86_64.whl
 
-    # Add a date after the dev0+ and before the hash in yyymmddHHMMSS format
-    # so pip will pick up the newest build. Try a little to make sure
+    # The date is added after the dev0+ and before the hash in yyymmddHHMMSS
+    # format so pip will pick up the newest build. Try a little to make sure
     # - the first part ends with 'dev0+'
     # - the second part starts with a lower case alphanumeric then a '-'
     # if those conditions are not met, the name will be returned as-is
